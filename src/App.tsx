47f9a955168daa2fb092c8f46dedd89b096c8e1e
Žiga Krašovec <zkrasovec@gmail.com>
2026-04-27 08:33:33 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,130 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<section id="center">
+				<div className="hero">
+					<img alt="" className="base" height="179" src={heroImg} width="170" />
+					<img alt="React logo" className="framework" src={reactLogo} />
+					<img alt="Vite logo" className="vite" src={viteLogo} />
+				</div>
+				<div>
+					<h1>Get started</h1>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+					</p>
+				</div>
+				<button
+					className="counter"
+					onClick={() => setCount((count) => count + 1)}
+					type="button"
+				>
+					Count is {count}
+				</button>
+			</section>
+
+			<div className="ticks"></div>
+
+			<section id="next-steps">
+				<div id="docs">
+					<svg aria-hidden="true" className="icon" role="presentation">
+						<use href="/icons.svg#documentation-icon"></use>
+					</svg>
+					<h2>Documentation</h2>
+					<p>Your questions, answered</p>
+					<ul>
+						<li>
+							<a href="https://vite.dev/" rel="noopener" target="_blank">
+								<img alt="" className="logo" src={viteLogo} />
+								Explore Vite
+							</a>
+						</li>
+						<li>
+							<a href="https://react.dev/" rel="noopener" target="_blank">
+								<img alt="" className="button-icon" src={reactLogo} />
+								Learn more
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div id="social">
+					<svg aria-hidden="true" className="icon" role="presentation">
+						<use href="/icons.svg#social-icon"></use>
+					</svg>
+					<h2>Connect with us</h2>
+					<p>Join the Vite community</p>
+					<ul>
+						<li>
+							<a
+								href="https://github.com/vitejs/vite"
+								rel="noopener"
+								target="_blank"
+							>
+								<svg
+									aria-hidden="true"
+									className="button-icon"
+									role="presentation"
+								>
+									<use href="/icons.svg#github-icon"></use>
+								</svg>
+								GitHub
+							</a>
+						</li>
+						<li>
+							<a href="https://chat.vite.dev/" rel="noopener" target="_blank">
+								<svg
+									aria-hidden="true"
+									className="button-icon"
+									role="presentation"
+								>
+									<use href="/icons.svg#discord-icon"></use>
+								</svg>
+								Discord
+							</a>
+						</li>
+						<li>
+							<a href="https://x.com/vite_js" rel="noopener" target="_blank">
+								<svg
+									aria-hidden="true"
+									className="button-icon"
+									role="presentation"
+								>
+									<use href="/icons.svg#x-icon"></use>
+								</svg>
+								X.com
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://bsky.app/profile/vite.dev"
+								rel="noopener"
+								target="_blank"
+							>
+								<svg
+									aria-hidden="true"
+									className="button-icon"
+									role="presentation"
+								>
+									<use href="/icons.svg#bluesky-icon"></use>
+								</svg>
+								Bluesky
+							</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<div className="ticks"></div>
+			<section id="spacer"></section>
+		</>
+	);
+}
+
+export default App;
